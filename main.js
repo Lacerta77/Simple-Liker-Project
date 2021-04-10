8b38bf69b63427fa4defd194397ebaa4bea027d5
lacerta <kevinhickman4@gmail.com>
2021-04-10 16:48:08 -0500

    used resorces to finish js

--- main.js
+++ main.js
@@ -16,28 +16,33 @@ const colorStates = {
   let articleHearts = document.querySelectorAll(".like-glyph");
   
   function likeCallback(e) {
-    let heart = e.target;
-    mimicServerCall()
+    const heart = e.target;
+    mimicServerCall("bogusUrl")
       .then(function(serverMessage){
-
-        alert("You notified the server!");
-         alert(serverMessage);
          heart.innerText = glyphStates[heart.innerText];
          heart.style.color = colorStates[heart.style.color];
       })
       .catch(function(error) {
-        alert("Something went wrong!");
+        const modal = document.getElementById("modal");
+        modal.className = "";
+        modal.innerText = error;
+        setTimeout(() =>  modal.className = "hidden", 3000);
       });
   }
- 
-  for (let glyph of articleHearts) {
-    glyph.addEventListener("click", likeCallback);
-    }
   
-  function mimicServerCall() {
-    return new Promise(function(resolve, reject) {
-      setTimeout(function() {
-        resolve("Pretend remote server notified of action!");
-      }, 300);
-    });
+  for (const glyph of articleHearts) {
+    glyph.addEventListener("click", likeCallback);
   }
+  
+  function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      const isRandomFailure = Math.random() < .2
+      if (isRandomFailure) {
+        reject("Something went wrong, try again.");
+      } else {
+        resolve("Pretend remote server notified of action!");
+      }
+    }, 300);
+  });
+}
