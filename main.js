298c42d7b10895b5656e6e2f02d30a68a8dbfa72
lacerta <kevinhickman4@gmail.com>
2021-04-10 16:29:59 -0500

    added error modal and started css

---
+++ main.js
@@ -0,0 +1,43 @@
+// We use this code, known as Objects, to control toggling like / unlike status
+
+const EMPTY_HEART = '♡'
+const FULL_HEART = '♥'
+
+const glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
+
+const colorStates = {
+  "red" : "",
+  "": "red"
+};
+  
+  let articleHearts = document.querySelectorAll(".like-glyph");
+  
+  function likeCallback(e) {
+    let heart = e.target;
+    mimicServerCall()
+      .then(function(serverMessage){
+
+        alert("You notified the server!");
+         alert(serverMessage);
+         heart.innerText = glyphStates[heart.innerText];
+         heart.style.color = colorStates[heart.style.color];
+      })
+      .catch(function(error) {
+        alert("Something went wrong!");
+      });
+  }
+ 
+  for (let glyph of articleHearts) {
+    glyph.addEventListener("click", likeCallback);
+    }
+  
+  function mimicServerCall() {
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        resolve("Pretend remote server notified of action!");
+      }, 300);
+    });
+  }
